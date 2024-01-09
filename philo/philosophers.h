b10e5871c66847f75c49232846b59d951eb61b54
Jonas Götz <jonas.goetz01@web.de>
2024-01-09 17:52:05 +0100

    rename

--- philo/philosophers.h
+++ philo/philosophers.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philosophers.h                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jgotz <jgotz@student.42heilbronn.de>       +#+  +:+       +#+        */
+/*   By: jgotz <jgotz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 16:51:59 by jgotz             #+#    #+#             */
-/*   Updated: 2023/12/05 17:03:20 by jgotz            ###   ########.fr       */
+/*   Updated: 2024/01/09 17:09:59 by jgotz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,18 @@
 # include <sys/time.h>
 # include <unistd.h>
 
-typedef struct s_args
+typedef struct args_s
 {
 	int	num_philo;
 	int	time_to_die;
 	int	time_to_eat;
 	int	time_to_sleep;
 	int	num_eat;
-}		t_args;
+}		args_t;
 
 void	error(char *str);
+
+void	usage(void);
 
 int		ft_atoi(const char *str);
 
