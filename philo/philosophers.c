b6015f0ad69b3257f9a856b169c09216f9ad42d7
Jonas Götz <jonas.goetz01@web.de>
2024-01-25 00:50:44 +0100

    reformat

--- philo/philosophers.c
+++ philo/philosophers.c
@@ -6,16 +6,16 @@
 /*   By: jgotz <jgotz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 16:51:15 by jgotz             #+#    #+#             */
-/*   Updated: 2024/01/09 17:50:48 by jgotz            ###   ########.fr       */
+/*   Updated: 2024/01/25 00:50:16 by jgotz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "philosophers.h"
+#include "./philosophers.h"
 #include <time.h>
 #define NUM_THREADS 2
 
-int mails = 0;
-pthread_mutex_t mutex;
+int				mails = 0;
+pthread_mutex_t	mutex;
 
 void	*routine(void *arg)
 {
@@ -29,7 +29,7 @@ void	*routine(void *arg)
 	return (NULL);
 }
 
-void input_parsing(int argc, char **argv)
+void	input_parsing(int argc, char **argv)
 {
 	t_args	args;
 
@@ -47,30 +47,27 @@ void input_parsing(int argc, char **argv)
 
 int	main(int argc, char **argv)
 {
-	args_t		args;
 	clock_t		start_time;
 	clock_t		end_time;
 	double		cpu_time_used;
 	pthread_t	th[NUM_THREADS];
 
-	input_parsing(argc, argv, );
-    start_time = clock();
+	input_parsing(argc, argv);
+	start_time = clock();
 	pthread_mutex_init(&mutex, NULL);
 	for (int i = 0; i < NUM_THREADS; i++)
 	{
 		if (pthread_create(&th[i], NULL, &routine, NULL) != 0)
 			error("Error creating thread");
 	}
-
 	for (int i = 0; i < NUM_THREADS; i++)
 	{
 		if (pthread_join(th[i], NULL) != 0)
 			error("Error joining thread");
 	}
-
 	pthread_mutex_destroy(&mutex);
 	printf("mails: %d\n", mails);
 	end_time = clock();
-    cpu_time_used = ((double) (end_time - start_time)) / CLOCKS_PER_SEC;
-    printf("CPU time used: %f seconds\n", cpu_time_used);
+	cpu_time_used = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
+	printf("CPU time used: %f seconds\n", cpu_time_used);
 }
--- philo/philosophers.h
+++ philo/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: jgotz <jgotz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 16:51:59 by jgotz             #+#    #+#             */
-/*   Updated: 2024/01/09 17:09:59 by jgotz            ###   ########.fr       */
+/*   Updated: 2024/01/17 12:33:10 by jgotz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,14 @@
 # include <sys/time.h>
 # include <unistd.h>
 
-typedef struct args_s
+typedef struct s_args
 {
 	int	num_philo;
 	int	time_to_die;
 	int	time_to_eat;
 	int	time_to_sleep;
 	int	num_eat;
-}		args_t;
+}		t_args;
 
 void	error(char *str);
 
